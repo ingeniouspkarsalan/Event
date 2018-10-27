604536587825c5b50cb2d55ddb27c72b4c470dc8
ingenioupkarsalan <ingeniou.aarsalan@gmail.com>
2018-10-27 13:54:40 +0500

    initial commit

---
+++ app/src/main/java/com/event/ingenious/event/Actvities/Splash.java
@@ -0,0 +1,15 @@
+package com.event.ingenious.event.Actvities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.event.ingenious.event.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/com/event/ingenious/event/Classes/Main_class.java
@@ -0,0 +1,4 @@
+package com.event.ingenious.event.Classes;
+
+public class Main_class {
+}
