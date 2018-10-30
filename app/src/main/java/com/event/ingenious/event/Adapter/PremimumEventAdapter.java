0c54eba4fa47d7805689678e1c2f3e47f995148b
DkKhan786 <ingenious.dilawar@gmail.com>
2018-10-30 17:49:04 +0500

    Premimum Event on Home page

---
+++ app/src/main/java/com/event/ingenious/event/Adapter/PremimumEventAdapter.java
@@ -0,0 +1,75 @@
+package com.event.ingenious.event.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.event.ingenious.event.Classes.Premimum_Event_Class;
+import com.event.ingenious.event.R;
+
+import java.util.List;
+
+public class PremimumEventAdapter extends RecyclerView.Adapter<PremimumEventAdapter.PEViewHolder>
+{
+    private Context context;
+    private List<Premimum_Event_Class> premimum_event_classList;
+
+    public PremimumEventAdapter(Context context, List<Premimum_Event_Class> premimum_event_classList) {
+        this.context = context;
+        this.premimum_event_classList = premimum_event_classList;
+    }
+
+    @Override
+    public PEViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.card_premimum_event, null);
+        return new PEViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return premimum_event_classList.size();
+    }
+
+    class PEViewHolder extends RecyclerView.ViewHolder {
+        CardView for_click;
+        ImageView banner_image;
+        TextView ev_title, ev_category, ev_address, ev_date, ev_desc;
+
+        public PEViewHolder(View itemView)
+        {
+            super(itemView);
+            for_click = (CardView) itemView.findViewById(R.id.card_view);
+            banner_image = (ImageView) itemView.findViewById(R.id.ev_banner_image);
+            ev_title = (TextView) itemView.findViewById(R.id.ev_title);
+            ev_category = (TextView) itemView.findViewById(R.id.ev_category);
+            ev_address = (TextView) itemView.findViewById(R.id.ev_address);
+            ev_date = (TextView) itemView.findViewById(R.id.ev_date);
+            ev_desc = (TextView) itemView.findViewById(R.id.ev_desc);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(PEViewHolder holder , int position)
+    {
+        final Premimum_Event_Class premimumEventClass = premimum_event_classList.get(position);
+        Glide.with(context)
+                .load(premimumEventClass.getEv_image())
+                .into(holder.banner_image);
+        holder.ev_title.setText(premimumEventClass.getEv_title());
+        holder.ev_category.setText(premimumEventClass.getEv_category());
+        holder.ev_address.setText(premimumEventClass.getEv_address());
+        holder.ev_date.setText(premimumEventClass.getEv_date());
+        holder.ev_desc.setText(premimumEventClass.getEv_desc());
+
+
+
+    }
+}
---
+++ app/src/main/java/com/event/ingenious/event/Classes/JSONParser.java
@@ -0,0 +1,50 @@
+package com.event.ingenious.event.Classes;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONParser
+{
+    static List<Premimum_Event_Class> premimum_event_classList;
+
+    public static List<Premimum_Event_Class> prese_premimum_event(String content)
+    {
+        JSONArray jsonArray = null;
+        Premimum_Event_Class premimum_event_class = null;
+        try
+        {
+            jsonArray = new JSONArray(content);
+            premimum_event_classList = new ArrayList<>();
+            for(int i = 0; i < jsonArray.length(); i++)
+            {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                premimum_event_class = new Premimum_Event_Class();
+
+                premimum_event_class.setEv_id(jsonObject.getString("ev_id"));
+                premimum_event_class.setEv_title(jsonObject.getString("ev_title"));
+                premimum_event_class.setEv_desc(jsonObject.getString("ev_desc"));
+                premimum_event_class.setEv_date(jsonObject.getString("ev_date"));
+                premimum_event_class.setEv_start_time(jsonObject.getString("ev_start_time"));
+                premimum_event_class.setEv_end_time(jsonObject.getString("ev_end_time"));
+                premimum_event_class.setEv_address(jsonObject.getString("ev_address"));
+                premimum_event_class.setEv_latitude(jsonObject.getString("ev_latitude"));
+                premimum_event_class.setEv_longitude(jsonObject.getString("ev_longitude"));
+                premimum_event_class.setEv_category(jsonObject.getString("ev_category"));
+                premimum_event_class.setEv_image(jsonObject.getString("ev_image"));
+                premimum_event_class.setEv_created_by(jsonObject.getString("ev_create_by"));
+
+                premimum_event_classList.add(premimum_event_class);
+            }
+            return premimum_event_classList;
+        }
+        catch (JSONException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+    }
+}
