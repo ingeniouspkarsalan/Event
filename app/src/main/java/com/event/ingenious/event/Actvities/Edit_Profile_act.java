1b6e4f7552239f621dba3b4bed1ce59b4866c066
ingenioupkarsalan <ingeniou.aarsalan@gmail.com>
2018-12-05 13:34:25 +0500

    update

--- app/src/main/java/com/event/ingenious/event/Actvities/Edit_Profile_act.java
+++ app/src/main/java/com/event/ingenious/event/Actvities/Edit_Profile_act.java
@@ -1,13 +1,31 @@
 package com.event.ingenious.event.Actvities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.event.ingenious.event.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.nex3z.togglebuttongroup.MultiSelectToggleGroup;
 import com.nex3z.togglebuttongroup.button.LabelToggle;
 
-public class Edit_Profile_act extends AppCompatActivity {
+public class Edit_Profile_act extends AppCompatActivity implements LocationListener {
+    EditText name, password, age, contact, locations;
+    Spinner gender;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +36,51 @@ public class Edit_Profile_act extends AppCompatActivity {
         for (String text : dummyText) {
             LabelToggle toggle = new LabelToggle(this);
             toggle.setText(text);
+            toggle.setChecked(true);
             multiDummy.addView(toggle);
         }
+        init();
+    }
+
+    private void init() {
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        name = findViewById(R.id.name);
+        password = findViewById(R.id.password);
+        age = findViewById(R.id.age);
+        contact = findViewById(R.id.contact);
+        locations = findViewById(R.id.location);
+        gender = findViewById(R.id.gender);
+        String[] genders = getResources().getStringArray(R.array.gender);
+        gender.setAdapter(new ArrayAdapter<String>(Edit_Profile_act.this, android.R.layout.simple_spinner_dropdown_item, genders));
+
+        locations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                
+            }
+        });
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
--- app/src/main/java/com/event/ingenious/event/Actvities/Sign_up.java
+++ app/src/main/java/com/event/ingenious/event/Actvities/Sign_up.java
@@ -77,7 +77,7 @@ public class Sign_up extends AppCompatActivity {
         });
     }
 
-    private void sign_up(String names,String contacts,String emails, String passwords,String token)
+    private void sign_up( String names, String contacts, String emails, String passwords, String token)
     {
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams params = new RequestParams();
@@ -109,7 +109,6 @@ public class Sign_up extends AppCompatActivity {
                             Toasty.success(Sign_up.this,object.getString("message"),Toast.LENGTH_LONG).show();
                             Prefs.putString("user_id",object.getString("id"));
                             Prefs.putString("user_name",object.getString("name"));
-                            Prefs.putString("user_email",object.getString("email"));
                             Prefs.putBoolean("loginSuccess",true); // change this value on logout
                             startActivity(new Intent(Sign_up.this, Home.class));
                             finish();
