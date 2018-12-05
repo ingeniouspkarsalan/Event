8207dc5a007a02e7e3d29ba807c2cac17e3d18e7
ingenioupkarsalan <ingeniou.aarsalan@gmail.com>
2018-12-05 11:56:49 +0500

    update

--- app/src/main/java/com/event/ingenious/event/Actvities/Edit_Profile_act.java
+++ app/src/main/java/com/event/ingenious/event/Actvities/Edit_Profile_act.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.event.ingenious.event.R;
+import com.nex3z.togglebuttongroup.MultiSelectToggleGroup;
+import com.nex3z.togglebuttongroup.button.LabelToggle;
 
 public class Edit_Profile_act extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class Edit_Profile_act extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit__profile_act);
+        MultiSelectToggleGroup multiDummy = findViewById(R.id.group_dummy);
+        String[] dummyText = getResources().getStringArray(R.array.dummy_text);
+        for (String text : dummyText) {
+            LabelToggle toggle = new LabelToggle(this);
+            toggle.setText(text);
+            multiDummy.addView(toggle);
+        }
     }
 }
